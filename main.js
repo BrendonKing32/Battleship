9838c0e18ebeb8d3490b880cc6e7dff22c4250d1
Brendon King <brendonking@outlook.com>
2018-01-19 10:22:21 -0700

    Overhaul UI
    Cleaned up the CSS to make the page more professional and readable.
    
    Updated the tables on gameboard.html to contain a game state for the battleship game.
    
    renamed contact.html to about.html (website does not require a contact page but does eventually require a page containing important details about the source code)

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
 function generate_table() {
-  // get the reference for the body
-  var body = document.getElementsByTagName("body")[0];
+    
+    // get the reference for the body
+    var body = document.getElementsByTagName("body")[0];
 
-  // creates a <table> element and a <tbody> element
-  var tbl = document.createElement("table");
-  var tblBody = document.createElement("tbody");
+    // creates a <table> element and a <tbody> element
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
 
-  // creating all cells
-  for (var i = 0; i < 8; i++) {
-    // creates a table row
-    var row = document.createElement("tr");
+    // creating all cells
+    for (var i = 0; i < 10; i++) {
+        // creates a table row
+        var row = document.createElement("tr");
 
-    for (var j = 0; j < 8; j++) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      var cell = document.createElement("td");
-      var cellText = document.createTextNode();
-      cell.appendChild(cellText);
-      row.appendChild(cell);
+        for (var j = 0; j < 10; j++) {
+            // Create a <td> element and a text node, make the text
+            // node the contents of the <td>, and put the <td> at
+            // the end of the table row
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode();
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        // add the row to the end of the table body
+        tblBody.appendChild(row);
     }
-
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-  }
-
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblBody);
-  // appends <table> into <body>
-  body.appendChild(tbl);
-  // sets the border attribute of tbl to 2;
-  tbl.setAttribute("border", "2");
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tbl);
+    // sets the border attribute of tbl to 2;
+    tbl.setAttribute("border", "2");
 }
