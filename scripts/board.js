b9fdbfb40be15c9cba2cab05b2af807d5ec0ae35
Brendon King <Brendonking@outlook.com>
2018-02-02 13:01:48 -0700

    Updated gameboard.html

--- scripts/board.js
+++ scripts/board.js
@@ -5,10 +5,10 @@ Title      :board.js
 Version    :Version 01
 Description:Contains the functions necessary to create game board and set up battleship.
 */
-function GenerateTable() {
+function GenerateTable1() {
 
     // get the reference for the body
-    var body = document.getElementsByTagName("body")[0];
+    var body = document.getElementsByTagName("Player1Board")[0];
 
     // creates a <table> element and a <tbody> element
     var tbl = document.createElement("table");
@@ -37,4 +37,43 @@ function GenerateTable() {
     body.appendChild(tbl);
     // sets the border attribute of tbl to 2;
     tbl.setAttribute("border", "2");
+}
+
+function GenerateTable2() {
+
+    // get the reference for the body
+    var body = document.getElementsByTagName("Player2Board")[0];
+
+    // creates a <table> element and a <tbody> element
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+
+    // creating all cells
+    for (var i = 0; i < 10; i++) {
+        // creates a table row
+        var row = document.createElement("tr");
+
+        for (var j = 0; j < 10; j++) {
+            // Create a <td> element and a text node, make the text
+            // node the contents of the <td>, and put the <td> at
+            // the end of the table row
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode();
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        // add the row to the end of the table body
+        tblBody.appendChild(row);
+    }
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tbl);
+    // sets the border attribute of tbl to 2;
+    tbl.setAttribute("border", "2");
+}
+
+function GenerateTable() {
+  GenerateTable1();
+  GenerateTable2();
 }
