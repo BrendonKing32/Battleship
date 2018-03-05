433ca9ff8d3b8ebad92381177e545639da003d8b
Brendon King <brendonking@outlook.com>
2018-03-04 22:22:19 -0700

    Updated documentation for model.js

--- scripts/game/model.js
+++ scripts/game/model.js
@@ -6,16 +6,21 @@ Version    :Version 03
 Description:Contains the functions that control game play and manage the data
 */
 
-//Function  :
-//Purpose   :
-//Parameters:
-//Returns   :
+//Function  :newGame()
+//Purpose   :constructor method for the game object
+//Parameters:none
+//Returns   :game object
 function newGame() {
     var game = initializeGame();
     return game;
 }
 
 // Objects
+
+//Function  :Game()
+//Purpose   :constructor method for the game.  Initializes all other variables
+//Parameters:grid, computerGrid, playerFleet, computerFleet
+//Returns   :none
 function Game(grid, computerGrid, playerFleet, computerFleet) {
     this.grid = grid;
     this.computerGrid = computerGrid;
@@ -26,6 +31,10 @@ function Game(grid, computerGrid, playerFleet, computerFleet) {
     this.computerCol = -1;
 }
 
+//Function  :PlayerFleet()
+//Purpose   :constructor method for the player ships
+//Parameters:none
+//Returns   :none
 function PlayerFleet() {
     this.placedCount = 0;
     this.playerCarrier;
@@ -36,7 +45,11 @@ function PlayerFleet() {
     this.shipsSunk = 0;
 }
 
-function ComputerShips() {
+//Function  :ComputerFleet
+//Purpose   :constructor method for the computer ships
+//Parameters:none
+//Returns   :none
+function ComputerFleet() {
     this.computerCarrier;
     this.computerBattleship;
     this.computerCruiser;
@@ -45,6 +58,10 @@ function ComputerShips() {
     this.shipsSunk = 0;
 }
 
+//Function  :Ship()
+//Purpose   :constructor method for individual ships
+//Parameters:size, location
+//Returns   :none
 function Ship(size, location) {
     this.shipSize = size;
     this.shipLocation = location;
@@ -53,11 +70,16 @@ function Ship(size, location) {
 }
 
 // Functions
+
+//Function  :initializeGame()
+//Purpose   :initializes game variables and overwrites old data
+//Parameters:none
+//Returns   :game
 function initializeGame() {
     var grid = createGridArray(10, 10);
     var computerGrid = createGridArray(10, 10);
     var playerFleet = new PlayerFleet();
-    var computerFleet = new ComputerShips();
+    var computerFleet = new ComputerFleet();
 
     // Generates Player ships
     playerFleet.playerCarrier = new Ship(5, []);
@@ -77,6 +99,10 @@ function initializeGame() {
     return game;
 }
 
+//Function  :createGridArray()
+//Purpose   :initialize grid objects for interaction
+//Parameters:x, y integers (for size)
+//Returns   :grid object
 function createGridArray(x, y) {
     var grid = new Array();
     for (var i = 0; i < x; i++) {
@@ -89,6 +115,11 @@ function createGridArray(x, y) {
 }
 
 // Implement these functions later on to update the Model
+
+//Function  :placeShip()
+//Purpose   :updates the location of the player ships with information from the view
+//Parameters:playerFleet array
+//Returns   :updated playerFleet array
 function placeShip(playerFleet) {
     var ship = document.getElementById('input-ship').value;
     var row = parseInt(document.getElementById('input-row').value) - 1;
@@ -126,6 +157,10 @@ function placeShip(playerFleet) {
     return playerFleet;
 }
 
+//Function  :markShipHit()
+//Purpose   :update ship damage once received from the view and controller
+//Parameters:ship, ships
+//Returns   :updated ship object
 function markShipHit(ship, ships) {
     if (ships[ship].hits < ships[ship].shipSize) {
         if (ships[ship].hits == ships[ship].shipSize - 1) {
@@ -142,11 +177,19 @@ function markShipHit(ship, ships) {
     return ships;
 }
 
+//Function  :markGridHit()
+//Purpose   :updates the grid object with information after receiving it from the view and controller
+//Parameters:row, col, grid
+//Returns   :updated grid
 function markGridHit(row, col, grid) {
     grid[col][row] = '<td class="hit"><b>HIT</b></td>';
     return grid;
 }
 
+//Function  :markGridMiss()
+//Purpose   :updates the grid object with information after receiving it from the view and controller
+//Parameters:row, col, grid
+//Returns   :updated grid
 function markGridMiss(row, col, grid) {
     grid[col][row] = '<td class="miss"><b>MISS</b></td>';
     return grid;
